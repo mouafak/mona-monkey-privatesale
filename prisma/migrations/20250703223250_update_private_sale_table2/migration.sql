ba511663fdae779d0ebd9d9842d79dccdc194691
mouafak <al-ali-mouafak@outlook.fr>
2025-07-04 15:51:18 +0200

    rest database - prisma

--- prisma/migrations/20250703223250_update_private_sale_table2/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `affiliatePercent` on the `privatesale` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `privatesale` DROP COLUMN `affiliatePercent`,
-    ADD COLUMN `affiliateUserProfit` VARCHAR(191) NULL;
---
+++ prisma/migrations/20250704135032_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE `PrivateSale` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `walletAddress` VARCHAR(191) NOT NULL,
+    `price` VARCHAR(191) NOT NULL DEFAULT '10000',
+    `solanaValue` VARCHAR(191) NOT NULL,
+    `tokenValue` VARCHAR(191) NOT NULL,
+    `txHash` MEDIUMTEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `affiliateCode` VARCHAR(191) NULL,
+    `affiliateUserProfit` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AffiliateUser` (
+    `affiliateCode` VARCHAR(191) NOT NULL,
+    `walletAddress` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `AffiliateUser_affiliateCode_key`(`affiliateCode`),
+    UNIQUE INDEX `AffiliateUser_walletAddress_key`(`walletAddress`),
+    PRIMARY KEY (`affiliateCode`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `PrivateSale` ADD CONSTRAINT `PrivateSale_affiliateCode_fkey` FOREIGN KEY (`affiliateCode`) REFERENCES `AffiliateUser`(`affiliateCode`) ON DELETE SET NULL ON UPDATE CASCADE;
